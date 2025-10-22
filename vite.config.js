00fe885916caa8a4f5e63a0c4caddb1601c7cc08
fiorella <fmilla1000@gmail.com>
2025-10-22 19:27:46 +0200

    fixes config base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     vue()
   ],
-  base: '/fiom2307.github.io/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
